0c768873230ea98a6156996cfd63df07f080fc30
03sid-sharma <03sid.sharma@gmail.com>
2021-05-13 13:36:20 +0530

    vue.js init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+import Vue from 'vue';
+Vue.component('follow-button',require('./components/FollowButton.vue').default);
+
+const app = new Vue({
+    el: '#app',
+})
---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button class="btn btn-primary ml-4">Follow</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted(){
+      console.log("Hello World");
+  }     
+};
+</script>
