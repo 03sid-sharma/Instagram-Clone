d3efe530ae027357fab74406dcf1ac171c7a11b5
03sid-sharma <03sid.sharma@gmail.com>
2021-05-13 14:27:35 +0530

    vue.js many to many finished

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
-    <button class="btn btn-primary ml-4">Follow</button>
+    <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText"></button>
   </div>
 </template>
 
 <script>
 
 export default {
-  mounted(){
-      console.log("Hello World");
-  }     
+  props:['userId','follows'],
+  
+  data: function(){
+    return {
+      status:this.follows,
+    }
+  },
+
+  methods:{
+    followUser(){
+      axios.post('/follow/' + this.userId)
+        .then(response => {
+          this.status = !this.status;
+          console.log(response.data);
+        })
+        .catch(errors => {
+          if(errors.response.status == 401){
+            window.location = '/login';
+          }  
+        });
+      
+      ;
+    }
+  },
+
+  computed: {
+    buttonText(){
+      return (this.status) ? 'Unfollow' : 'Follow';
+    }
+  }
 };
 </script>
