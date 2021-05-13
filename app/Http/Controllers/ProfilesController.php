f9537221d0c58acdebf4774a528689098760c0e9
03sid-sharma <03sid.sharma@gmail.com>
2021-05-13 12:07:15 +0530

    auto create user

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use \App\Models\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -12,10 +13,14 @@ class ProfilesController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update', $user->profile);
+        
         return view('profiles.edit',compact('user'));
     }
 
     public function update(User $user){
+        $this->authorize('update', $user->profile);
+
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -23,10 +28,19 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
         
-        auth()->user()->profile->update($data);
-
         
+        if(request('image')){
+            $imagePath = request('image')->store('profile','public');
+            
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+            $image->save();
+        }
 
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath],
+        ));
+        
         return redirect("/profile/{$user->id}");
     }
 }
