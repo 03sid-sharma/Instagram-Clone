e063cd59f4db759b0ff22b9ffe2a7d288438a84c
03sid-sharma <03sid.sharma@gmail.com>
2021-05-13 16:28:42 +0530

    project finished

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome To fiestaGram !!
+
+This is a community of fellow developers and we love that you've joined us.
+
+
+All the best,<br>
+Siddharth
+@endcomponent
